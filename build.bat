17d2eb130ff31a8e53345c03296b156b66998a0d
Pierce Brooks <piercebrooks@rocketmail.com>
2023-01-24 23:37:26 -0500

    extra cmake inclusion directories and flags

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ vcpkg.exe install boost-python:x64-windows
 cd ..
 mkdir build
 cd build
-cmake -A x64 -DTARGET_PLATFORM=%1 -DCMAKE_INSTALL_PREFIX=../pyomvbb/omvbb -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake -A x64 -DTARGET_PLATFORM=%1 -DCMAKE_INSTALL_PREFIX=../pyomvbb/omvbb -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DApproxMVBB_BUILD_LIBRARY_STATIC=OFF ..
 cmake --build . --target install
 cd ..
 
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DTARGET_PLATFORM=$1 -DPY_VERSION=$2 -DPython3_ROOT_DIR=$3 -DPython_ROOT_DIR=$3 -DPython_EXECUTABLE=$4 -DCMAKE_INSTALL_PREFIX=../pyomvbb/omvbb ..
+cmake -G "Unix Makefiles" -DTARGET_PLATFORM=$1 -DPY_VERSION=$2 -DPython3_ROOT_DIR=$3 -DPython_ROOT_DIR=$3 -DPython_EXECUTABLE=$4 -DCMAKE_INSTALL_PREFIX=../pyomvbb/omvbb -DApproxMVBB_BUILD_LIBRARY_STATIC=ON ..
 cmake --build . --target install
 cd ..
 
